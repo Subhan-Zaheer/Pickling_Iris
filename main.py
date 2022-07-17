c5e395cbeb91b59bab3fd84c1f4867a2f737a0d4
Subhan-Zaheer <2020cs666@student.uet.edu.pk>
2022-07-18 02:05:23 +0500

    File is pickled

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 import pickle
+import requests
+import json
+url = "https://newsapi.org/v2/top-headlines?q=cricket&from=2022-07-15&to=2022-07-15&sortBy=popularity&apiKey=a23441b217ed489b8350cf8ca4ba317f"
+text1 = requests.get(url).text
+print(text1)
+print(type(text1))
+parsedText_here = json.loads(text1)
+articles = parsedText_here['articles']
+file = "articles.pkl"
+fileobj = open(file, "wb")
+ls = []
+for items in articles:
+    ls.append(items)
 
+print(ls)
+pickle.dump(ls, fileobj)
+fileobj.close()
